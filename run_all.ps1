2ebd9ef49d73b0dc3b1b1bb66867c4b830b0b4c5
LabyrinthCoder <archenginecode@gmail.com>
2026-05-18 22:47:07 -0400

    Delete run_all.ps1
    Signed-off-by: LabyrinthCoder <archenginecode@gmail.com>

--- run_all.ps1
+++
@@ -1,8 +0,0 @@
-# run_all.ps1
-# Chains: unblock → handshake → gatecheck → ledger append → sysinfo → quickcheck
-
-# Make sure scripts aren't marked as downloaded
-Get-ChildItem . -Filter *.ps1 | Unblock-File
-
-# Execution
-.\ve_syscheck.ps1
