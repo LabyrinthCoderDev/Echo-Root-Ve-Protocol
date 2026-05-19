3fcd651680aa28e1d0ffe3ba41510ceec8a13c4d
LabyrinthCoder <archenginecode@gmail.com>
2026-05-18 22:46:59 -0400

    Delete ve.ps1
    Signed-off-by: LabyrinthCoder <archenginecode@gmail.com>

--- ve.ps1
+++
@@ -1,5 +0,0 @@
-﻿param([Parameter(ValueFromRemainingArguments=\True)] [string[]]\)
-. "$PSScriptRoot\ve_kernel.ps1"
-Import-Module "$PSScriptRoot\Modules\VE.Guard\VE.Guard.psd1" -Force
-\ = \ -join ' '
-Invoke-Child \
