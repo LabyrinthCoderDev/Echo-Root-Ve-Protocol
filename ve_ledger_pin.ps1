9218660ef7f01a5915d34c2ba42df3c0d9974c7f
LabyrinthCoder <archenginecode@gmail.com>
2026-05-18 22:55:01 -0400

    Delete ve_ledger_pin.ps1
    Signed-off-by: LabyrinthCoder <archenginecode@gmail.com>

--- ve_ledger_pin.ps1
+++
@@ -1,6 +0,0 @@
-﻿param(
-    [string]$LedgerPath = ".\ve_ledger.jsonl"
-)
-$pinned = '{"ts":"2025-10-31T02:50:00.0000000-04:00","actor":"VE_Helper","action":"init-ledger","hash_prev":"","hash_self":"94e596ff62a914031377843be88b3eae01e69cf8bba1dfcc43e7fceba4709546"}'
-$pinned | Set-Content -Encoding UTF8 -NoNewline $LedgerPath
-Write-Host "✅ pinned genesis written to $LedgerPath"
